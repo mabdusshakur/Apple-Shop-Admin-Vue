a8d7ddc392da2fbfeac7aaa760c0729d71bcc260
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-26 15:19:13 +0600

    bootstrap,jquery-data-table CDN disabled

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,14 +15,14 @@
 
     <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css') }}" rel="stylesheet" />
 
-    <link href="{{ asset('admin/css/jquery.dataTables.min.css') }}" rel="stylesheet" />
+    {{-- <link href="{{ asset('admin/css/jquery.dataTables.min.css') }}" rel="stylesheet" />
     <script src="{{ asset('admin/js/jquery-3.7.0.min.js') }}"></script>
-    <script src="{{ asset('admin/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin/js/jquery.dataTables.min.js') }}"></script> --}}
 
     <script src="{{ asset('admin/js/toastify-js.js') }}"></script>
     <script src="{{ asset('admin/js/axios.min.js') }}"></script>
     <script src="{{ asset('admin/js/config.js') }}"></script>
-    <script src="{{ asset('admin/js/bootstrap.bundle.js') }}"></script>
+    {{-- <script src="{{ asset('admin/js/bootstrap.bundle.js') }}"></script> --}}
 
     {{-- For The Client Side Auth --}}
     {{-- <script src=" https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js "></script> --}}
