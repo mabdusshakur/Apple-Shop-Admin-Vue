7ae9ee0e909d368f4ef27ada2c540ebbc553748a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-27 14:57:43 +0600

    axios instance imported

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
-window.axios = axios;
+import axiosInstance from "./axios";
+window.axios = axiosInstance;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
